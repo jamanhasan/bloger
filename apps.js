01bb73df0d353ecaa01b274593db2dbb0293719a
jamanhasan <81922680+jamanhasan@users.noreply.github.com>
2021-07-10 13:27:32 +0600

    Update apps.js

--- apps.js
+++ apps.js
@@ -5,9 +5,9 @@ sidenav.style.top = ("-400px")
 menubtn.onclick = function () {
     if (sidenav.style.top == "-400px") {
         sidenav.style.top = "100px"
-        menu.src = "/images/icon-close.svg";
+        menu.src = "images/icon-close.svg";
     } else {
         sidenav.style.top = "-400px";
-        menu.src = "/images/icon-hamburger.svg";
+        menu.src = "images/icon-hamburger.svg";
     }
 }
