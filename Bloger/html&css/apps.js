b28998490d4ef87ed0e8dbb5da2f4bbabc98e1cb
jamanhasan <81922680+jamanhasan@users.noreply.github.com>
2021-07-10 12:33:49 +0600

    blogr

---
+++ Bloger/html&css/apps.js
@@ -0,0 +1,13 @@
+var menubtn = document.getElementById("menu-btn");
+var sidenav = document.getElementById("side-nav");
+var menu = document.getElementById("menu");
+sidenav.style.top = ("-400px")
+menubtn.onclick = function () {
+    if (sidenav.style.top == "-400px") {
+        sidenav.style.top = "100px"
+        menu.src = "/images/icon-close.svg";
+    } else {
+        sidenav.style.top = "-400px";
+        menu.src = "/images/icon-hamburger.svg";
+    }
+}
